13422de27f85c6edc5fa92ca1d316da262bc4413
tsagueyves21 <tsagueyvesthibaut@gmail.com>
2022-04-18 16:31:49 +0200

    update database

--- src/main/java/com/jwt/ApiContact1Application.java
+++ src/main/java/com/jwt/ApiContact1Application.java
@@ -39,9 +39,9 @@ public class ApiContact1Application implements CommandLineRunner {
 //		 userService.saveUser(new UserModel("tomy","chuck","chuck@gmail.com" ,Sexe.FEMME,"1234" ,"OIPCRMTFTRB.jpg",Status_value.ACTIVE,new ArrayList<>(),new ArrayList<>()));
 //		 userService.saveUser(new UserModel("sarra","morgan","morgan@gmail.com",Sexe.FEMME,"1234" ,"OIPW7RJF7LH.jpg",Status_value.ACTIVE,new ArrayList<>(),new ArrayList<>()));
 
-//		 userService.saveRole(new Role_Model("ROLE_USER"));
-//		 userService.saveRole(new Role_Model("ROLE_ADMIN"));
-//		 userService.saveRole(new Role_Model("ROLE_SUPER_ADMIN"));
+		 userService.saveRole(new Role_Model("ROLE_USER"));
+		 userService.saveRole(new Role_Model("ROLE_ADMIN"));
+		 userService.saveRole(new Role_Model("ROLE_SUPER_ADMIN"));
 //		 
 //		 userService.addRoleToUser("case", "ROLE_USER");
 //		 userService.addRoleToUser("yves", "ROLE_SUPER_ADMIN");
