8c9e234bbae9cb6b8e49a9f36c655c37cc0ccf18
tsagueyves21 <tsagueyvesthibaut@gmail.com>
2022-04-18 16:12:13 +0200

    premiere modification

--- src/main/java/com/jwt/config/CrossConfig.java
+++ src/main/java/com/jwt/config/CrossConfig.java
@@ -14,7 +14,7 @@ public class CrossConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				        .allowedOrigins(" http://localhost:4200")
+				        .allowedOrigins("http://localhost:4200")
 				        .allowedMethods("POST","GET","PUT","DELETE","PACH")
 				        .allowedHeaders("*")
 				        .exposedHeaders("Authorization")
